5a8f3a126aecee4054931cccefeccf3d010488f7
Thiago Marques <thiagoftsm@gmail.com>
2021-03-02 16:26:51 +0000

    ebpf_mount: Add ebpf program

---
+++ includes/netdata_mount.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef _NETDATA_MOUNT_H_
+#define _NETDATA_MOUNT_H_ 1
+
+enum mount_counters {
+    NETDATA_KEY_MOUNT_CALL,
+    NETDATA_KEY_MOUNT_ERROR,
+    NETDATA_KEY_UMOUNT_CALL,
+    NETDATA_KEY_UMOUNT_ERROR,
+
+    NETDATA_SYNC_END
+};
+
+#endif /* _NETDATA_MOUNT_H_ */
