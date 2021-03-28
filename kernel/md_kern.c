40752bea994a3e471902d412637a9adc35941843
Thiago Marques <thiagoftsm@gmail.com>
2021-03-28 21:09:29 +0000

    ebpf_xfs: multiple cpu

--- kernel/md_kern.c
+++ kernel/md_kern.c
@@ -12,7 +12,11 @@
  ***********************************************************************************/
 
 struct bpf_map_def SEC("maps") tbl_sync = {
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)) 
     .type = BPF_MAP_TYPE_HASH,
+#else
+    .type = BPF_MAP_TYPE_PERCPU_HASH,
+#endif    
     .key_size = sizeof(__u32),
     .value_size = sizeof(__u64),
     .max_entries = NETDATA_MD_END
