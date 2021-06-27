cc9b8273943db194da95f39b2caf8a2f709b589c
Thiago Marques <thiagoftsm@gmail.com>
2021-06-27 13:35:41 +0000

    ebpf_everything: Add compatibility

--- includes/netdata_disk.h
+++ includes/netdata_disk.h
@@ -29,21 +29,14 @@ struct netdata_block_rq_complete {
     unsigned int nr_sector;     // offset:24;      size:4; signed:0;
     int error;                  // offset:28;      size:4; signed:1;
     char rwbs[8];               // offset:32;      size:8; signed:1;
-    int data_loc_name;          // offset:40;      size:4; signed:1; ()https://lists.linuxfoundation.org/pipermail/iovisor-dev/2017-February/000627.html
+    int data_loc_name;          // offset:40;      size:4; signed:1; 
+                                //(https://lists.linuxfoundation.org/pipermail/iovisor-dev/2017-February/000627.html)
 };
 
 typedef struct netdata_disk_key {
     dev_t dev;
     sector_t sector;
 } netdata_disk_key_t;
-
-typedef struct netdata_bootsector {
-    u64 start_sector;
-    u64 end_sector;
-    u64 timestamp;
-    u64 changed_sector;
-    u64 size;
-} netdata_bootsector_t;
 
 typedef struct block_key {
     __u32 bin;
